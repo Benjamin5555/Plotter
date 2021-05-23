9cdf929c7173c65bfd6185470766937d9c2ab516
Benjamin Carpenter <benjamincarpenter52@hotmail.co.uk>
2021-05-23 21:15:40 +0100

    Added ability for code to restart the animation script if an error occurs, reducing read/write errors

--- gplotter.py
+++ gplotter.py
@@ -1,23 +1,26 @@
 from subprocess import PIPE, Popen
 import pexpect
+import os
+
 
 def send_gnuplot_msg(msg_str):
     gnuplot_c.send(msg_str+"\n")
     gnuplot_c.expect("gnuplot>")
-    print(str(gnuplot_c.before))
+    #print(str(gnuplot_c.before))
     
 def read_script_file(filePath):
     with open(filePath, 'r') as file:
         return file.read()
 
-animate_script = read_script_file("animate")
-print(animate_script)
-
+animate_script = read_script_file(os.getcwd()+"/Plotter/animate")
 gnuplot_c = pexpect.spawn("gnuplot")
-gnuplot_c.expect("gnuplot>")
-send_gnuplot_msg(animate_script)
+while(True): #gnuplot can crash animation occasionally, this restarts the animation if it hangs
+    send_gnuplot_msg(animate_script)
+    gnuplot_c.expect("gnuplot>")
+
 """
 print(str(gnuplot_c.before))
+
 send_gnuplot_msg("set terminal x11 noraise\n")
 send_gnuplot_msg("set datafile separator whitespace\n")
 send_gnuplot_msg("set view map\n")
