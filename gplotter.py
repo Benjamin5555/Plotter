6aa58894a1f922a3b81b581245b97c94b3c7cfcb
Benjamin Carpenter <benjamincarpenter52@hotmail.co.uk>
2021-05-30 16:37:07 +0100

    Gplotter update

--- gplotter.py
+++ gplotter.py
@@ -3,20 +3,52 @@ import pexpect
 import os
 
 
-def send_gnuplot_msg(msg_str):
-    gnuplot_c.send(msg_str+"\n")
-    gnuplot_c.expect("gnuplot>")
-    #print(str(gnuplot_c.before))
-    
-def read_script_file(filePath):
-    with open(filePath, 'r') as file:
-        return file.read()
-
 animate_script = read_script_file(os.getcwd()+"/Plotter/animate")
-gnuplot_c = pexpect.spawn("gnuplot")
-while(True): #gnuplot can crash animation occasionally, this restarts the animation if it hangs
-    send_gnuplot_msg(animate_script)
-    gnuplot_c.expect("gnuplot>")
+
+
+class gPlot():
+    def __init__(self):
+        gnuplot_c = pexpect.spawn("gnuplot")
+
+    def send_gnuplot_msg(self,msg_str):
+        gnuplot_c.send(msg_str+"\n")
+        gnuplot_c.expect("gnuplot>")
+        #print(str(gnuplot_c.before))
+        
+    def read_script_file(self,filePath):
+        with open(filePath, 'r') as file:
+            return file.read()
+    
+    def run_script(self,script_filepath):
+        send_gnuplot_msg("l '"+str(script_filepath)+"'")
+         
+        if(gnuplot_c.expect(["expecting filename","gnuplot>"])==0)#Maybe should be == expecting filename"
+            raise FileNotFoundError("Filename invalid")
+        else:
+            return
+    
+    
+    def animate_lattice(self):
+        """
+            Animates lattice stored in data.dat file as a matrix (i.e. 2x2 array stored with each 
+            position being the vlue at that position (i.e. form saved by np.savetxt())
+        """
+        try:
+            while(True): #gnuplot can crash animation occasionally, this restarts the animation if it hangs
+                send_gnuplot_msg(animate_script)
+                gnuplot_c.expect("gnuplot>")
+        except KeyboardInterrupt:
+                gnuplot_c.expect("gnuplot>")
+    
+
+
+    def run_gnuplot_command(self,cmd):
+        self.send_gnuplot_msg(cmd+"\n")
+        self.gnuplot_c.expect("gnuplot>")
+
+
+
+
 
 """
 print(str(gnuplot_c.before))
